7e93ddf5aff671917d94f5b847536c2c164a6619
Alexey <alexey.bakulin96@gmail.com>
2021-09-29 01:24:46 +0300

    [edit]: variables names in main have been changed

--- src/by/epamtc/bakulin/task01/runner/Task01Runner.java
+++ src/by/epamtc/bakulin/task01/runner/Task01Runner.java
@@ -28,25 +28,25 @@ public class Task01Runner extends AbstractConsoleInput {
     public static void runTask01_2() {
         printConsoleMessage("Task 2: Started...");
         printConsoleMessage("Enter a numeric integer year and month, to calculate days quantity");
-        int year = enterInteger("year");
-        int month = enterInteger("month");
-        int result = CalculateDaysAtLeapYearMonth.calculateDaysAtLeapYearMonth(year, month);
+        int userYear = enterInteger("year");
+        int userMonth = enterInteger("month");
+        int result = CalculateDaysAtLeapYearMonth.calculateDaysAtLeapYearMonth(userYear, userMonth);
         printConsoleMessage(String.format("Task 2: result = %d \n", result));
     }
 
     public static void runTask01_3() {
         printConsoleMessage("Task 3: Started...");
         printConsoleMessage("Enter numeric double outer rectangular square, to calculate rectangle inscribed in a circle, which inscribed in first rectangular");
-        double outerRectangleSquare = enterDouble("outer rectangle square");
-        double innerRectangleSquare = CalculateRectangleSquareInscribedInCircle.calculateRectangleSquareInCircle(outerRectangleSquare);
+        double userOuterRectangleSquare = enterDouble("outer rectangle square");
+        double innerRectangleSquare = CalculateRectangleSquareInscribedInCircle.calculateRectangleSquareInCircle(userOuterRectangleSquare);
         printConsoleMessage(String.format("Task 3: result = %.3f \n", innerRectangleSquare));
     }
 
     public static void runTask01_4() {
         printConsoleMessage("Task 4: Started...");
         printConsoleMessage("Enter 4 integer values and check - are there at list 2 or more even values. True - has 2 and more even values.");
-        int[] intArray = populateIntegerArray(4);
-        boolean isHasTwoEvenNumbers = IsIntegerArrayHasTwoEvenNumerics.isIntegerArrayHasTwoEvenNumerics(intArray);
+        int[] userIntegerArray = populateIntegerArray(4);
+        boolean isHasTwoEvenNumbers = IsIntegerArrayHasTwoEvenNumerics.isIntegerArrayHasTwoEvenNumerics(userIntegerArray);
         printConsoleMessage(String.format("Task 4: result = %b \n", isHasTwoEvenNumbers));
     }
 
