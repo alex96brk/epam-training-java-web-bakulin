bd648b981a44b8522c0439cd3804e732b80008f4
Alexey <alexey.bakulin96@gmail.com>
2021-09-30 18:29:30 +0300

    [edit]: ComparePointsLocation, main() removed

--- src/by/epamtc/bakulin/task01/service/ComparePointsLocation.java
+++ src/by/epamtc/bakulin/task01/service/ComparePointsLocation.java
@@ -9,15 +9,6 @@ public class ComparePointsLocation {
     * 6. Даны две точки А(х1, у1) и В(х2, у2). Составить алгоритм, определяющий, которая из точек находится ближе к началу координат. x y.
     * */
 
-    public static void main(String[] args) {
-        PlainPoint centerPoint = new PlainPoint("O", 0, 0);
-        PlainPoint firstPoint = new PlainPoint("A", 3, 1);
-        PlainPoint secondPoint = new PlainPoint("B", 1, 3);
-        PlainPoint result = comparePointsToCenter(centerPoint, firstPoint, secondPoint);
-        System.out.println(result);
-
-    }
-
     public static PlainPoint comparePointsToCenter(PlainPoint center, PlainPoint firstPoint, PlainPoint secondPoint) {
         PlainPoint result = secondPoint;
         double firstPointToCenterLength = Math.sqrt(Math.pow(center.getX() - firstPoint.getX(), 2) + Math.pow(center.getY() - firstPoint.getY(), 2));
