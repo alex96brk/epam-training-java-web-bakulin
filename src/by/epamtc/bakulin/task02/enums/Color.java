aff55e119b2085e6c0c704a521f198c61ed6fc22
Alexey <alexey.bakulin96@gmail.com>
2021-10-04 14:58:09 +0300

    [edit]: .gitignore edit

---
+++ src/by/epamtc/bakulin/task02/enums/Color.java
@@ -0,0 +1,17 @@
+package by.epamtc.bakulin.task02.entity;
+
+import java.io.Serializable;
+
+public enum Color implements Serializable {
+    RED("Red"), BLUE("Blue"), GREEN("Green"), YELLOW("Yellow");
+
+    private String colorName;
+
+    Color(String color) {
+        this.colorName = color;
+    }
+
+    public String getColorName() {
+        return colorName;
+    }
+}
